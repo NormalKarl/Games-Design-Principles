5dc0de2fd7a521e1e71f171e3ad469ef4d90fba7
Karl Ward <karlward97@gmail.com>
2018-05-18 11:00:00 +0100

    Improved camera and added additional 'device'

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour {
+
+    private Canvas canvas;
+    public CameraMovement CameraController;
+    public GameObject OnStartRemoveObject;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GetComponent<Canvas>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playAction() {
+        canvas.enabled = false;
+
+        if (OnStartRemoveObject != null)
+            Destroy(OnStartRemoveObject);
+
+        if (CameraController != null)
+        {
+            CameraController.ResetCamera();
+            CameraController.Playing = true;
+        }
+    }
+
+    public void exitAction()
+    {
+        Application.Quit();
+    }
+
+}
