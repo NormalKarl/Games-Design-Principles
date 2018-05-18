68816e8156dccd03c196efe9a99b743095fe3be3
Karl Ward <karlward97@gmail.com>
2018-05-18 11:56:38 +0100

    Added elements and adjusted paddle speeds.

--- Assets/Scripts/Paddles.cs
+++ Assets/Scripts/Paddles.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Paddles : MonoBehaviour {
 
+    public CameraMovement CameraController;
     public float RotationSpeed; 
 
 	// Update is called once per frame
 	void Update () {
-        Transform t = GetComponent<Transform>();
+        //Chekc if camera controller is not null and playing.
+        if(CameraController != null && CameraController.Playing)
+        {
+            //t.localRotation.SetAxisAngle(new Vector3(0, 0, 1), t.localRotation.z + RotationSpeed);
+            //t.localRotation.Set(0, 0, t.localRotation.z + RotationSpeed, 0);
+            Quaternion quat = transform.localRotation;
+            Vector3 eul = quat.eulerAngles;
+            eul.Set(0, 0, transform.localRotation.eulerAngles.z + RotationSpeed);
+            quat.eulerAngles = eul;
+            transform.localRotation = quat;
+        }
 
-        //t.localRotation.SetAxisAngle(new Vector3(0, 0, 1), t.localRotation.z + RotationSpeed);
-        //t.localRotation.Set(0, 0, t.localRotation.z + RotationSpeed, 0);
-        Quaternion quat = t.localRotation;
-        Vector3 eul = quat.eulerAngles;
-        eul.Set(0, 0, t.localRotation.eulerAngles.z + RotationSpeed);
-        quat.eulerAngles = eul;
-        t.localRotation = quat;
 
     }
 }
